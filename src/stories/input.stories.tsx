a44b33b5482174ddbecf2a44f65961adbf52153f
antonorlov <fogelo@yandex.ru>
2022-03-18 15:16:20 +0300

    13. controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -10,11 +10,6 @@ export const UncontrolledInput = () => {
         <input type="text"/>
     )
 }
-export const ControlledInput = () => {
-    return (
-        <input value={'hard'} type="text"/>
-    )
-}
 
 export const AddInputValue = () => {
     const [value, setValue] = useState('')
@@ -23,11 +18,31 @@ export const AddInputValue = () => {
         <>
             <input ref={inputRef} type="text"/>
             <button onClick={() => {
-                const el = inputRef.current as  HTMLInputElement
+                const el = inputRef.current as HTMLInputElement
                 setValue(el.value)
             }}>save
             </button>
             {value}
         </>
+    )
+}
+
+export const ControlledInput = () => {
+    const [value, setValue] = useState('')
+    return <input value={value} onChange={(e) => setValue(e.currentTarget.value)} type="text"/>
+}
+export const ControlledCheckbox = () => {
+    const [value, setValue] = useState(true)
+    return <input checked={value} onChange={(e) => setValue(e.currentTarget.checked)} type="checkbox"/>
+}
+export const ControlledSelect = () => {
+    const [value, setValue] = useState('2')
+    return (
+        <select value={value} onChange={(e) => setValue(e.currentTarget.value)}>
+            <option>none</option>
+            <option value="1">minsk</option>
+            <option value="2">moscow</option>
+            <option value="3">kiev</option>
+        </select>
     )
 }
